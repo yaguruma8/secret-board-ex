d012c93542c936389b148c183948b178a6816111
yaguruma <yaguruma_y@yahoo.co.jp>
2021-04-06 19:28:31 +0900

    仮データの整形

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const contents = ['おはよう', 'こんにちは', 'さようなら'];
+const posts = [
+  { id: 1, name: 'taro', content: 'おはようございます。', date: '2021/1/1' },
+  { id: 2, name: 'hanako', content: '今日も一日元気です。', date: '2021/2/2' },
+  { id: 3, name: 'jiro', content: 'ありがとう。', date: '2021/3/3' },
+];
 
 /*
  /posts
@@ -20,7 +24,7 @@ const contents = ['おはよう', 'こんにちは', 'さようなら'];
 
 // 投稿一覧の表示
 router.get('/', function (req, res, next) {
-  res.send('this is posts page.');
+  res.render('posts', { posts: posts });
 });
 
 // 投稿
@@ -30,6 +34,5 @@ router.post('/', (req, res, next) => {
   contents.push(req.body.content);
   res.redirect('/posts');
 });
-
 
 module.exports = router;
