caafa47013ccfa73802fad1baaeaf8fae49f754d
yaguruma <yaguruma_y@yahoo.co.jp>
2021-04-08 19:19:16 +0900

    [update]#1 DB接続したため投稿データをDBから取得するように変更

--- routes/posts.js
+++ routes/posts.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../model/postModel');
 
-const posts = [
-  { id: 1, name: 'taro', content: 'おはようございます。', date: '2021/1/1' },
-  { id: 2, name: 'hanako', content: '今日も一日元気です。', date: '2021/2/2' },
-  { id: 3, name: 'jiro', content: 'ありがとう。', date: '2021/3/3' },
-];
+// const posts = [
+//   { id: 1, name: 'taro', content: 'おはようございます。', date: '2021/1/1' },
+//   { id: 2, name: 'hanako', content: '今日も一日元気です。', date: '2021/2/2' },
+//   { id: 3, name: 'jiro', content: 'ありがとう。', date: '2021/3/3' },
+// ];
 
 /*
- /posts
-  GET 投稿一覧表示
-  posts.pug
 
  /posts/add
   POST 新規投稿 /posts にリダイレクト
@@ -22,9 +20,11 @@ const posts = [
 
 */
 
-// 投稿一覧の表示
+// GET 投稿一覧表示
 router.get('/', function (req, res, next) {
-  res.render('posts', { posts: posts });
+  Post.findAll().then((posts) => {
+    res.render('posts', { posts: posts, user: req.user });
+  });
 });
 
 // 投稿
