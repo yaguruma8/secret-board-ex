b0efa24cd8c2750060679f433c7f0ed6438ea6ab
yaguruma <yaguruma_y@yahoo.co.jp>
2021-04-11 21:10:25 +0900

    [update]#6 ログアウト時に認証を解除するための処理を追加

--- routes/logout.js
+++ routes/logout.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 /*
   /logout
-    GET Basic認証の解除 2-3秒後に / へリダイレクト
+    GET 
+    Basic認証の解除
+    todo:2-3秒後に / へリダイレクト
     logout.pug 
 */
 
 // ログアウト
 router.get('/', function (req, res, next) {
-  res.render('logout');
+  res.status(401).render('logout');
 });
 
 module.exports = router;
